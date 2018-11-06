8d99705d1a6cd983bc64c4fe0345c7a1a5837e4b
tianfu.huang <tianfu.huang@samsung.com>
2018-11-06 16:35:51 +0800

    Add LED controller test program with software control style

---
+++ apps/examples/esp32_demo/esp32_ledc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <tinyara/ledc.h>
+#include <fcntl.h>
+
+/* select gpio pin4 as ledc pwm output, connect to LED power*/
+
+void test_ledc_soft(void)
+{
+    int fd1;
+    struct ledc_info_s ledc_info;
+    printf("test_ledc_soft\n");
+    fd1 = open("/dev/ledc", O_RDWR);
+    if (fd1 < 0) {
+        printf("fd open fail\n");
+        return -1 ;
+    }   
+    ledc_info.frequency = 1000; 
+    ledc_info.ishwfade = 0;
+    int dutys1[] = {256, 1024, 4096, 8191};
+
+    for( int i = 0; i < 4; i++)
+    {   
+        ledc_info.duty = dutys1[i];  
+        ioctl(fd1, LEDCIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&ledc_info));
+        ioctl(fd1, LEDCIOC_START);
+        printf("current level : %d\n",i+1);
+        up_mdelay(300);
+    }   
+    ioctl(fd1, LEDCIOC_STOP);
+    close(fd1);
+
+}
+
+
---
+++ os/arch/xtensa/src/esp32_DevKit/src/esp32_led.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <debug.h>
+
+#include <tinyara/ledc.h>
+#include "esp32_ledc.h"
+
+FAR struct ledc_lowerhalf_s *esp32_ledcinitialize(int i);
+
+int board_ledc_setup(void)
+{
+    FAR struct ledc_lowerhalf_s *ledc;
+    char path[10];
+    int ret = 0;
+    int i;
+    for (i = 0; i < 1; i++) {
+    
+        ledc = esp32_ledcinitialize(i);
+        if (!ledc) {
+            printf("Failed to get the PWM lower half\n");
+            return -ENODEV;
+        }   
+        
+        snprintf(path, sizeof(path), "/dev/ledc%d", i); 
+
+        printf("path = %s\n", path);
+
+        ret = ledc_register(path, ledc);
+        if (ret < 0) {
+            printf("ledc_register failed: %d\n", ret);
+            return ret;
+        }   
+    }   
+
+    return OK; 
+}
