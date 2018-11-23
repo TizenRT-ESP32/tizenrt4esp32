1c0d54d2ede2982bfd3a423ef58c3b6556654d58
longbao.guo <longbao.guo@samsung.com>
2018-11-24 02:32:34 +0800

    Add ADC initiallization to board

--- os/arch/xtensa/src/esp32_DevKit/src/esp32_boot.c
+++ os/arch/xtensa/src/esp32_DevKit/src/esp32_boot.c
@@ -71,6 +71,11 @@
 #include "esp32_i2c.h"
 #include <tinyara/gpio.h>
 
+#if defined(CONFIG_ADC)
+#include "esp32_adc.h"
+#include <tinyara/analog/adc.h>
+#endif
+
 /************************************************************************************
  * Pre-processor Definitions
  ************************************************************************************/
@@ -265,6 +270,42 @@ static void board_i2c_initialize(void)
 #endif
 }
 
+static void board_adc_initialize(void)
+{
+#if defined(CONFIG_ADC)
+    static bool adc_initialized;
+    struct adc_dev_s* adc;
+    const adc_channel_t chanlist[] = {
+        ADC_CHANNEL_4,
+        ADC_CHANNEL_5,
+        ADC_CHANNEL_6,
+        ADC_CHANNEL_7
+    };
+
+    /* Have we already initialized? */
+    if (!adc_initialized) {
+
+        adc = esp32_adc_initialize(chanlist, sizeof(chanlist)/sizeof(adc_channel_t));
+        if (NULL != adc) {
+            lldbg("ERROR: Failed to get the ESP32 ADC driver\n");
+        }
+        else {
+            lldbg("ERROR: up_spiinitialize failed\n"t);
+        }
+
+        /* Register the ADC driver at "/dev/adc0" */
+        int ret = adc_register("/dev/adc0", adc);
+        if (ret < 0) {
+            return;
+        }
+
+        /* Now we are initialized */
+
+        adc_initialized = true;
+    }
+#endif
+}
+
 /****************************************************************************
  * Name: board_initialize
  *
@@ -288,5 +329,7 @@ void board_initialize(void)
 	board_gpio_initialize();
 	board_i2c_initialize();
 	board_ledc_setup();
+
+	board_adc_initialize();
 }
 #endif
