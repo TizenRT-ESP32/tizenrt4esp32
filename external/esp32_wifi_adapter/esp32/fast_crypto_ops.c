0288400f905f7a69b700b379085aed1755996999
tianfu.huang <tianfu.huang@samsung.com>
2019-01-09 12:07:59 +0800

    Fix WPA 4-way handshake issue using software aes wrap.

--- external/esp32_wifi_adapter/esp32/fast_crypto_ops.c
+++ external/esp32_wifi_adapter/esp32/fast_crypto_ops.c
@@ -38,8 +38,8 @@
 const wpa_crypto_funcs_t g_wifi_default_wpa_crypto_funcs = {
     .size = sizeof(wpa_crypto_funcs_t), 
     .version = ESP_WIFI_CRYPTO_VERSION,
-    .aes_wrap = (esp_aes_wrap_t)fast_aes_wrap,
-    .aes_unwrap = (esp_aes_unwrap_t)fast_aes_unwrap,
+    .aes_wrap = (esp_aes_wrap_t)aes_wrap,
+    .aes_unwrap = (esp_aes_unwrap_t)aes_unwrap,
     .hmac_sha256_vector = (esp_hmac_sha256_vector_t)fast_hmac_sha256_vector,
     .sha256_prf = (esp_sha256_prf_t)fast_sha256_prf,
     .hmac_md5 = (esp_hmac_md5_t)hmac_md5,
